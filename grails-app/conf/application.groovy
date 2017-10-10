dd6979d296f8da7f6f709fe886a70468873e99c1
nasulla <khalid_khalid@hotmail.co.uk>
2017-10-10 12:54:36 +0100

    Added additional static rules

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -14,7 +14,9 @@ grails.plugin.springsecurity.controllerAnnotations.staticRules = [
 	[pattern: '/**/js/**',       access: ['permitAll']],
 	[pattern: '/**/css/**',      access: ['permitAll']],
 	[pattern: '/**/images/**',   access: ['permitAll']],
-	[pattern: '/**/favicon.ico', access: ['permitAll']]
+	[pattern: '/**/favicon.ico', access: ['permitAll']],
+    [pattern: '/user/**',        access: 'ROLE_ADMIN'],
+    [pattern: '/role/**',        access: 'ROLE_ADMIN']
 ]
 
 grails.plugin.springsecurity.filterChain.chainMap = [
